e45188f3dfeaf6fc8006bc41131b98ca9671da6e
Aditya Saklani <adisaklani@gmail.com>
2024-10-20 23:02:43 +0530

    add login and signup files

--- task1/task1/urls.py
+++ task1/task1/urls.py
@@ -21,4 +21,6 @@ from welcomeApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home.html"),
+    path('login/', views.loginView, name='login'),
+    path('signup/', views.signup,name='signup'),
 ]
