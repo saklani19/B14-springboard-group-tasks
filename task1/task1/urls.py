faec7ae315dd978d767dd288bfd5cfa73d6afb0d
Aditya Saklani <adisaklani@gmail.com>
2024-11-17 22:16:29 +0530

    alter views.py, urls.py and settings.py, and enable uploads of images on profile.html

--- task1/task1/urls.py
+++ task1/task1/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from welcomeApp import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
@@ -25,3 +28,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- task1/welcomeApp/views.py
+++ task1/welcomeApp/views.py
@@ -1,6 +1,11 @@
+import cv2
+import os
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate
+from django.core.files.storage import FileSystemStorage
+
 
 # Create your views here.
 def home(request):
@@ -47,4 +52,34 @@ def signup(request):
         return render(request,'signup.html',{'form':form})
 
 def profile(request):
-    return render(request,'profile.html')
+    # initialise url variables
+    img_url = None
+    processed_img_url = None
+
+    if request.method == "POST":
+        if request.FILES.get('uploadImage'):
+            img_name = request.FILES['uploadImage']
+            fs = FileSystemStorage()
+            filename = fs.save(img_name.name, img_name)
+            img_url = fs.url(filename)
+
+            uploaded_img_path = fs.path(filename)
+            processed_img_path = process_image_with_opencv(uploaded_img_path)
+
+            processed_filename = processed_img_path.split('/')[-1]
+            processed_fs = FileSystemStorage()
+            processed_fs.save(processed_filename, open(processed_img_path, 'rb'))
+            processed_img_url = processed_fs.url(processed_filename)
+
+    return render(request, 'profile.html', {'img': img_url, 'processed_img': processed_img_url})
+
+def process_image_with_opencv(input_image_path):
+    img = cv2.imread(input_image_path)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    base_name, ext = os.path.splitext(input_image_path)
+    processed_image_path = f"{base_name}_processed{ext}"
+    
+    cv2.imwrite(processed_image_path, gray_img)
+
+    return processed_image_path
